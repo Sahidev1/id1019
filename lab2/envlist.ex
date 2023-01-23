1e59d3d69c8d30f14ef1324b5850474696084449
Sahidev1 <theoddrogue@gmail.com>
2023-01-23 22:34:25 +0100

    code simplified

--- lab2/envlist.ex
+++ lab2/envlist.ex
@@ -1,15 +1,17 @@
 defmodule EnvList do
+  def testMap() do
+    m = add(nil, 4, :x)
+    m = add(m, 11, :y)
+    m = add(m, 7, :z)
+    m = add(m, 1, :v)
+    m
+  end
+
   def add(nil, key, value) do [{key, value}] end
   def add([], key, value) do [{key, value}] end
-  def add(map, key, value) do
-    [h|t] = map
-    {hkey,_} = h
-    cond do
-      key < hkey -> [{key, value}|map]
-      key > hkey -> [h|add(t, key, value)]
-      key === hkey -> [{key, value}|t]
-    end
-  end
+  def add([{key, _}|t], key, value) do [{key, value}|t] end
+  def add([{hkey,v}|t], key, value) when key < hkey do [{key, value}|[{hkey,v}|t]] end
+  def add([{hkey, v}| t], key, value) do [{hkey,v}|add(t, key, value)] end
 
   def lookup([], _) do nil end
   def lookup([{key, val}|_], key) do {key, val} end
