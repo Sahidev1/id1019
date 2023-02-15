78080f8bed6adb5f14b54ce3234972ecd62c9283
Sahidev1 <theoddrogue@gmail.com>
2023-02-15 23:25:03 +0100

    save point

--- lab5/advent_of_code_day16/Aoc.ex
+++ lab5/advent_of_code_day16/Aoc.ex
@@ -15,6 +15,11 @@ defmodule Aoc do
     {first, m}
   end
 
+  def run_test(t) do
+    {f, m} = gen_data();
+    traverse_graph([],f,m,0,0,t)
+  end
+
   def map_graph(map, []) do map end
   def map_graph(map, [{v, rest}|t]) do
     new_map = map_graph(map, t)
@@ -25,21 +30,25 @@ defmodule Aoc do
     {flow_tot, flowrate, path}
   end
   def traverse_graph(path, curr_vertex, graph_map, flowrate, flow_tot, time) do
+    IO.inspect({curr_vertex ,time, path, flow_tot, flowrate})
     {flow, edges} = Map.get(graph_map, curr_vertex, nil)
-    ret_not_open = Enum.map(edges, fn edge -> traverse_graph(path, edge, graph_map, flowrate, flow_tot + flowrate, time - 1) end)
+    ret_not_open = Enum.map(edges, fn edge -> traverse_graph(path++[{curr_vertex, :closed}], edge, graph_map, flowrate, flow_tot + flowrate, time - 1) end)
     {fl, flr, fpath} = Enum.max_by(ret_not_open, fn {flow_tot, _, _} -> flow_tot end)
-    #IO.puts("wowza")
-    if flow !== 0 do
-      ret_open = Enum.map(edges, fn edge -> traverse_graph(path, edge, graph_map, flowrate + flow, flow_tot + 2 * flowrate, time - 2) end)
-      #IO.puts("jammy")
+    if flow !== 0 and !is_valve_open(curr_vertex, path) do
+      ret_open = Enum.map(edges, fn edge -> traverse_graph(path++[{curr_vertex, :opened}], edge, graph_map, flowrate + flow, flow_tot + 2 * flowrate, time - 2) end)
       {fo, rate, opath} = Enum.max_by(ret_open, fn {ft, _, _} -> ft end)
       if fo > fl do
-        {fo, rate, path++[curr_vertex]++opath}
+        {fo, rate, opath}
       else
-        {fl, flr, path++[curr_vertex]++fpath}
+        {fl, flr, fpath}
       end
     else
-      {fl, flr, path++[curr_vertex]++fpath}
+      {fl, flr,fpath}
     end
   end
+
+  def is_valve_open(v,[]) do false end
+  def is_valve_open(v,[{v, :opened}|t]) do true end
+  def is_valve_open(v, [{v, :closed}|t]) do false end
+  def is_valve_open(v, [{dv, _}|t]) do is_valve_open(v, t) end
 end
