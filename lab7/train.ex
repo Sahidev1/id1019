34cba9aad2e0472a5cb221b1d5e930b3132557d7
Sahidev1 <theoddrogue@gmail.com>
2023-02-27 22:26:45 +0100

    improved code

--- lab7/train.ex
+++ lab7/train.ex
@@ -6,11 +6,9 @@ defmodule Train do
   def take([h|t], n) do [h|take(t, n - 1)] end
 
   def drop(train, 0) do train end
-  def drop([h|t], 1) do t end
-  def drop([h|t], n) do drop(t, n - 1) end
+  def drop([_|t], n) do drop(t, n - 1) end
 
-  def append([],[]) do [] end
-  def append([], [h|t]) do [h|append([],t)] end
+  def append([], train1) do train1 end
   def append([h|t], train1) do [h|append(t, train1)] end
 
   def member([], _) do false end
@@ -19,11 +17,11 @@ defmodule Train do
 
   def position([], y) do raise("atom "<>":"<>to_string(y)<>" is not a wagon of the train") end
   def position([y|_], y) do 1 end
-  def position([x|t], y) do position(t, y) + 1 end
+  def position([_|t], y) do position(t, y) + 1 end
 
   def split([], y) do raise("atom "<>":"<>to_string(y)<>" is not a wagon of the train") end
-  def split(train=[y|t], y) do {[], t} end
-  def split(train=[x|t], y) do
+  def split([y|t], y) do {[], t} end
+  def split([x|t], y) do
     {left_t, right_t} = split(t, y)
     {[x|left_t], right_t}
   end
