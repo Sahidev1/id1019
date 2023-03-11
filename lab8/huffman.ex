479bc08faf8f4571da9568e04fb7222699167610
Sahidev1 <theoddrogue@gmail.com>
2023-03-11 21:17:43 +0100

    huffman builder done

--- lab8/huffman.ex
+++ lab8/huffman.ex
@@ -1,5 +1,8 @@
 
 defmodule Huffman do
+  @type huff()::{{huff(), huff()}, integer()}
+  | {char(), integer()}
+
   def sample do
     'the quick brown fox jumps over the lazy dog
     this is a sample text that we will use when we build
@@ -13,17 +16,50 @@ defmodule Huffman do
   end
 
   def test do
-    sample = sample()
+    sample = text()
     tree = tree(sample)
-    encode = encode_table(tree)
-    decode = decode_table(tree)
-    text = text()
-    seq = encode(text, encode)
-    decode(seq, decode)
+    #encode = encode_table(tree)
+    #decode = decode_table(tree)
+    #text = text()
+    #seq = encode(text, encode)
+    #decode(seq, decode)
   end
 
   def tree(sample) do
-    # To implement...
+    prio_q = freq(sample)
+    huffman(prio_q)
+  end
+
+  def huffman(tree) when length(tree) === 1 do tree end
+  def huffman(tree) do
+    {tree, h0={e0, min0}} = getmin(tree)
+    {tree, h1={e1, min1}} = getmin(tree)
+
+    tree = [{{h0, h1}, min0 + min1}|tree]
+    #IO.inspect(tree)
+    huffman(tree)
+  end
+
+  def getmin([]) do {[], -1} end
+  def getmin(prio_q=[e|t]) do getmin(prio_q, e) end
+  def getmin([], m) do {[], m} end
+  def getmin([e={huff, c0}|q], m={_,c1}) when c0 < c1 do
+    {upd_q, min}=getmin(q, e)
+    case min do
+      ^e->
+        {upd_q, min}
+      _->
+        {[e|upd_q], min}
+    end
+  end
+  def getmin([e|q], m) do
+    {upd_q, min} = getmin(q, m)
+    case min do
+      ^e->
+        {upd_q, min}
+      _->
+        {[e|upd_q], min}
+    end
   end
 
   def encode_table(tree) do
